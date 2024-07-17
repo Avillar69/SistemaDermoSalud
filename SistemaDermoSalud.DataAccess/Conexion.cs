0bf728d4efb2323f95c2139a0a651d6f22aa266e
Developer Manager <developerdevopsmanager@hotmail.com>
2024-07-17 10:16:30 -0500

    layout

--- SistemaDermoSalud.DataAccess/Conexion.cs
+++ SistemaDermoSalud.DataAccess/Conexion.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq; 
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,10 +11,7 @@ namespace SistemaDermoSalud.DataAccess
     {
         public SqlConnection conectar()
         {
-            SqlConnection cn = new SqlConnection("Server=.\\SQLEXPRESS;Database=BD_ENUA;User ID=sa;Password=sql;Trusted_Connection=False;MultipleActiveResultSets=True");//BD para desarrollo Click Dermo
-            //SqlConnection cn = new SqlConnection("Server=186.64.122.205;Database=Dermosalud;User ID=sa;Password=sqlclick;Trusted_Connection=False;MultipleActiveResultSets=True");
-            //SqlConnection cn = new SqlConnection("Server=192.168.1.96;Database=Dermosalud;User ID=sa;Password=C3s4r4r4n4;Trusted_Connection=False;MultipleActiveResultSets=True");//estaba aqui
-            //SqlConnection cn = new SqlConnection("Server=.\\SQLEXPRESS;Database=Dermosalud;User ID=sa;Password=sql;Trusted_Connection=False;MultipleActiveResultSets=True");
+            SqlConnection cn = new SqlConnection("Server=143.137.145.154;Database=BD_ENUA;User ID=sa;Password=gpsL1429x;Trusted_Connection=False;MultipleActiveResultSets=True");//BD para desarrollo Click Dermo
             return cn;
         }
     }
